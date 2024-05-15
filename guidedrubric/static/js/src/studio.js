979821d84758b1b9c86723f59981372b8c64a3c5
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2024-05-15 06:42:27 +0000

    Eliminated assistant fields as and relevant logic as per the requirement

--- guidedrubric/static/js/src/studio.js
+++ guidedrubric/static/js/src/studio.js
@@ -107,17 +107,12 @@ function GuidedRubricXBlock(runtime, element) {
 
 
         var form_data = new FormData();
-        let assistant_name = $(element).find('input[name=assistant_name]').val();
-        let assistant_instructions = $(element).find('input[name=assistant_instructions]').val();
-        let assistant_model = $(element).find('input[name=assistant_model]').val();
-        let knowledge_base = $(element).find('#knowledge_base').prop('files')[0];
+        let assistant_id = $(element).find('input[name=assistant_id]').val();
         let completion_message = $(element).find('input[name=completion_message]').val();
         let max_tokens_per_user = $(element).find('input[name=max_tokens_per_user]').val();
         let completion_token = $(element).find('input[name=completion_token]').val();
 
-        form_data.append('assistant_instructions', assistant_instructions);
-        form_data.append('assistant_model', assistant_model);
-        form_data.append('knowledge_base', knowledge_base);
+        form_data.append('assistant_id', assistant_id);
         form_data.append('completion_message', completion_message);
         form_data.append('max_tokens_per_user', max_tokens_per_user);
         form_data.append('completion_token', completion_token);
@@ -159,7 +154,6 @@ function GuidedRubricXBlock(runtime, element) {
             
         });
 
-        form_data.append('assistant_name', assistant_name);
         form_data.append('phases', JSON.stringify(block_phases));
         form_data.append('last_phase_id', component_last_phase_id);
         runtime.notify('save', {
